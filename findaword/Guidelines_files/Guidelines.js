abb4bdc0a94f2be17aeaf086cecd555568744a0a
Mike Rossetti <vex@trymike.com>
2019-09-04 15:29:34 -0600

    Finish up One Word Single

--- findaword/Guidelines_files/Guidelines.js
+++ findaword/Guidelines_files/Guidelines.js
@@ -3,36 +3,288 @@
 function applyEffects() {
 	var registry=IWCreateEffectRegistry();
 	registry.registerEffects( { 
-		stroke_3:new
-		IWStrokeParts( [ {
-				rect:new IWRect(-2,2,4,329),
-				url:'Guidelines_files/stroke_24.png'
-			},{
-				rect:new IWRect(-2,-2,4,4),
-				url:'Guidelines_files/stroke_25.png'
-			},{
-				rect:new IWRect(2,-2,268,4),
-				url:'Guidelines_files/stroke_26.png'
-			},{
-				rect:new IWRect(270,-2,4,4),
-				url:'Guidelines_files/stroke_27.png'
-			},{
-				rect:new IWRect(270,2,4,329),
-				url:'Guidelines_files/stroke_28.png'
-			},{
-				rect:new IWRect(270,331,4,4),
-				url:'Guidelines_files/stroke_29.png'
-			},{
-				rect:new IWRect(2,331,268,4),
-				url:'Guidelines_files/stroke_30.png'
-			},{
-				rect:new IWRect(-2,331,4,4),
-				url:'Guidelines_files/stroke_31.png'
-			}
-		],
-		new IWSize(272,333)
-	),
-	stroke_5:new IWStrokeParts([{rect:new IWRect(-2,2,4,152),url:'Guidelines_files/stroke_40.png'},{rect:new IWRect(-2,-2,4,4),url:'Guidelines_files/stroke_41.png'},{rect:new IWRect(2,-2,218,4),url:'Guidelines_files/stroke_42.png'},{rect:new IWRect(220,-2,4,4),url:'Guidelines_files/stroke_43.png'},{rect:new IWRect(220,2,4,152),url:'Guidelines_files/stroke_44.png'},{rect:new IWRect(220,154,4,4),url:'Guidelines_files/stroke_45.png'},{rect:new IWRect(2,154,218,4),url:'Guidelines_files/stroke_46.png'},{rect:new IWRect(-2,154,4,4),url:'Guidelines_files/stroke_47.png'}],new IWSize(222,156)),stroke_6:new IWStrokeParts([{rect:new IWRect(-2,2,4,142),url:'Guidelines_files/stroke_48.png'},{rect:new IWRect(-2,-2,4,4),url:'Guidelines_files/stroke_49.png'},{rect:new IWRect(2,-2,268,4),url:'Guidelines_files/stroke_50.png'},{rect:new IWRect(270,-2,4,4),url:'Guidelines_files/stroke_51.png'},{rect:new IWRect(270,2,4,142),url:'Guidelines_files/stroke_52.png'},{rect:new IWRect(270,144,4,4),url:'Guidelines_files/stroke_53.png'},{rect:new IWRect(2,144,268,4),url:'Guidelines_files/stroke_54.png'},{rect:new IWRect(-2,144,4,4),url:'Guidelines_files/stroke_55.png'}],new IWSize(272,146)),stroke_4:new IWStrokeParts([{rect:new IWRect(-2,2,4,363),url:'Guidelines_files/stroke_32.png'},{rect:new IWRect(-2,-2,4,4),url:'Guidelines_files/stroke_33.png'},{rect:new IWRect(2,-2,268,4),url:'Guidelines_files/stroke_34.png'},{rect:new IWRect(270,-2,4,4),url:'Guidelines_files/stroke_35.png'},{rect:new IWRect(270,2,4,363),url:'Guidelines_files/stroke_36.png'},{rect:new IWRect(270,365,4,4),url:'Guidelines_files/stroke_37.png'},{rect:new IWRect(2,365,268,4),url:'Guidelines_files/stroke_38.png'},{rect:new IWRect(-2,365,4,4),url:'Guidelines_files/stroke_39.png'}],new IWSize(272,367)),stroke_8:new IWStrokeParts([{rect:new IWRect(-2,2,4,502),url:'Guidelines_files/stroke_64.png'},{rect:new IWRect(-2,-2,4,4),url:'Guidelines_files/stroke_65.png'},{rect:new IWRect(2,-2,267,4),url:'Guidelines_files/stroke_66.png'},{rect:new IWRect(269,-2,4,4),url:'Guidelines_files/stroke_67.png'},{rect:new IWRect(269,2,4,502),url:'Guidelines_files/stroke_68.png'},{rect:new IWRect(269,504,4,4),url:'Guidelines_files/stroke_69.png'},{rect:new IWRect(2,504,267,4),url:'Guidelines_files/stroke_70.png'},{rect:new IWRect(-2,504,4,4),url:'Guidelines_files/stroke_71.png'}],new IWSize(271,506)),stroke_9:new IWStrokeParts([{rect:new IWRect(-2,2,4,251),url:'Guidelines_files/stroke_72.png'},{rect:new IWRect(-2,-2,4,4),url:'Guidelines_files/stroke_73.png'},{rect:new IWRect(2,-2,270,4),url:'Guidelines_files/stroke_74.png'},{rect:new IWRect(272,-2,4,4),url:'Guidelines_files/stroke_75.png'},{rect:new IWRect(272,2,4,251),url:'Guidelines_files/stroke_76.png'},{rect:new IWRect(272,253,4,4),url:'Guidelines_files/stroke_77.png'},{rect:new IWRect(2,253,270,4),url:'Guidelines_files/stroke_78.png'},{rect:new IWRect(-2,253,4,4),url:'Guidelines_files/stroke_79.png'}],new IWSize(274,255)),stroke_7:new IWStrokeParts([{rect:new IWRect(-2,2,4,161),url:'Guidelines_files/stroke_56.png'},{rect:new IWRect(-2,-2,4,4),url:'Guidelines_files/stroke_57.png'},{rect:new IWRect(2,-2,273,4),url:'Guidelines_files/stroke_58.png'},{rect:new IWRect(275,-2,4,4),url:'Guidelines_files/stroke_59.png'},{rect:new IWRect(275,2,4,161),url:'Guidelines_files/stroke_60.png'},{rect:new IWRect(275,163,4,4),url:'Guidelines_files/stroke_61.png'},{rect:new IWRect(2,163,273,4),url:'Guidelines_files/stroke_62.png'},{rect:new IWRect(-2,163,4,4),url:'Guidelines_files/stroke_63.png'}],new IWSize(277,165)),stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,398),url:'Guidelines_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'Guidelines_files/stroke_1.png'},{rect:new IWRect(2,-2,268,4),url:'Guidelines_files/stroke_2.png'},{rect:new IWRect(270,-2,4,4),url:'Guidelines_files/stroke_3.png'},{rect:new IWRect(270,2,4,398),url:'Guidelines_files/stroke_4.png'},{rect:new IWRect(270,400,4,4),url:'Guidelines_files/stroke_5.png'},{rect:new IWRect(2,400,268,4),url:'Guidelines_files/stroke_6.png'},{rect:new IWRect(-2,400,4,4),url:'Guidelines_files/stroke_7.png'}],new IWSize(272,402)),stroke_1:new IWStrokeParts([{rect:new IWRect(-2,2,4,368),url:'Guidelines_files/stroke_8.png'},{rect:new IWRect(-2,-2,4,4),url:'Guidelines_files/stroke_9.png'},{rect:new IWRect(2,-2,268,4),url:'Guidelines_files/stroke_10.png'},{rect:new IWRect(270,-2,4,4),url:'Guidelines_files/stroke_11.png'},{rect:new IWRect(270,2,4,368),url:'Guidelines_files/stroke_12.png'},{rect:new IWRect(270,370,4,4),url:'Guidelines_files/stroke_13.png'},{rect:new IWRect(2,370,268,4),url:'Guidelines_files/stroke_14.png'},{rect:new IWRect(-2,370,4,4),url:'Guidelines_files/stroke_15.png'}],new IWSize(272,372)),stroke_2:new IWStrokeParts([{rect:new IWRect(-2,2,4,247),url:'Guidelines_files/stroke_16.png'},{rect:new IWRect(-2,-2,4,4),url:'Guidelines_files/stroke_17.png'},{rect:new IWRect(2,-2,268,4),url:'Guidelines_files/stroke_18.png'},{rect:new IWRect(270,-2,4,4),url:'Guidelines_files/stroke_19.png'},{rect:new IWRect(270,2,4,247),url:'Guidelines_files/stroke_20.png'},{rect:new IWRect(270,249,4,4),url:'Guidelines_files/stroke_21.png'},{rect:new IWRect(2,249,268,4),url:'Guidelines_files/stroke_22.png'},{rect:new IWRect(-2,249,4,4),url:'Guidelines_files/stroke_23.png'}],new IWSize(272,251))});registry.applyEffects();}
+		stroke_3:new IWStrokeParts( [ {
+					rect:new IWRect(-2,2,4,329),
+					url:'Guidelines_files/stroke_24.png'
+				},{
+					rect:new IWRect(-2,-2,4,4),
+					url:'Guidelines_files/stroke_25.png'
+				},{
+					rect:new IWRect(2,-2,268,4),
+					url:'Guidelines_files/stroke_26.png'
+				},{
+					rect:new IWRect(270,-2,4,4),
+					url:'Guidelines_files/stroke_27.png'
+				},{
+					rect:new IWRect(270,2,4,329),
+					url:'Guidelines_files/stroke_28.png'
+				},{
+					rect:new IWRect(270,331,4,4),
+					url:'Guidelines_files/stroke_29.png'
+				},{
+					rect:new IWRect(2,331,268,4),
+					url:'Guidelines_files/stroke_30.png'
+				},{
+					rect:new IWRect(-2,331,4,4),
+					url:'Guidelines_files/stroke_31.png'
+				}
+			],
+			new IWSize(272,333)
+		),
+		stroke_5:new IWStrokeParts( [ {
+					rect:new IWRect(-2,2,4,152),
+					url:'Guidelines_files/stroke_40.png'
+				},{
+					rect:new IWRect(-2,-2,4,4),
+					url:'Guidelines_files/stroke_41.png'
+				},{
+					rect:new IWRect(2,-2,218,4),
+					url:'Guidelines_files/stroke_42.png'
+				},{
+					rect:new IWRect(220,-2,4,4),
+					url:'Guidelines_files/stroke_43.png'
+				},{
+					rect:new IWRect(220,2,4,152),
+					url:'Guidelines_files/stroke_44.png'
+				},{
+					rect:new IWRect(220,154,4,4),
+					url:'Guidelines_files/stroke_45.png'
+				},{
+					rect:new IWRect(2,154,218,4),
+					url:'Guidelines_files/stroke_46.png'
+				},{
+					rect:new IWRect(-2,154,4,4),
+					url:'Guidelines_files/stroke_47.png'}
+			],
+			new IWSize(222,156)
+		),
+		stroke_6:new IWStrokeParts( [ {
+					rect:new IWRect(-2,2,4,142),
+					url:'Guidelines_files/stroke_48.png'
+				},{
+					rect:new IWRect(-2,-2,4,4),
+					url:'Guidelines_files/stroke_49.png'
+				},{
+					rect:new IWRect(2,-2,268,4),
+					url:'Guidelines_files/stroke_50.png'
+				},{
+					rect:new IWRect(270,-2,4,4),
+					url:'Guidelines_files/stroke_51.png'
+				},{
+					rect:new IWRect(270,2,4,142),
+					url:'Guidelines_files/stroke_52.png'
+				},{
+					rect:new IWRect(270,144,4,4),
+					url:'Guidelines_files/stroke_53.png'
+				},{
+					rect:new IWRect(2,144,268,4),
+					url:'Guidelines_files/stroke_54.png'
+				},{
+					rect:new IWRect(-2,144,4,4),
+					url:'Guidelines_files/stroke_55.png'
+				}
+			],
+			new IWSize(272,146)
+		),
+		stroke_4:new IWStrokeParts( [ {
+					rect:new IWRect(-2,2,4,363),
+					url:'Guidelines_files/stroke_32.png'
+				},{
+					rect:new IWRect(-2,-2,4,4),
+					url:'Guidelines_files/stroke_33.png'
+				},{
+					rect:new IWRect(2,-2,268,4),
+					url:'Guidelines_files/stroke_34.png'
+				},{
+					rect:new IWRect(270,-2,4,4),
+					url:'Guidelines_files/stroke_35.png'
+				},{
+					rect:new IWRect(270,2,4,363),
+					url:'Guidelines_files/stroke_36.png'
+				},{
+					rect:new IWRect(270,365,4,4),
+					url:'Guidelines_files/stroke_37.png'
+				},{
+					rect:new IWRect(2,365,268,4),
+					url:'Guidelines_files/stroke_38.png'
+				},{
+					rect:new IWRect(-2,365,4,4),
+					url:'Guidelines_files/stroke_39.png'
+				}
+			],
+			new IWSize(272,367)
+		),
+		stroke_8:new IWStrokeParts( [ {
+					rect:new IWRect(-2,2,4,502),
+					url:'Guidelines_files/stroke_64.png'
+				},{
+					rect:new IWRect(-2,-2,4,4),
+					url:'Guidelines_files/stroke_65.png'
+				},{
+					rect:new IWRect(2,-2,267,4),
+					url:'Guidelines_files/stroke_66.png'
+				},{
+					rect:new IWRect(269,-2,4,4),
+					url:'Guidelines_files/stroke_67.png'
+				},{
+					rect:new IWRect(269,2,4,502),
+					url:'Guidelines_files/stroke_68.png'
+				},{
+					rect:new IWRect(269,504,4,4),
+					url:'Guidelines_files/stroke_69.png'
+				},{
+					rect:new IWRect(2,504,267,4),
+					url:'Guidelines_files/stroke_70.png'
+				},{
+					rect:new IWRect(-2,504,4,4),
+					url:'Guidelines_files/stroke_71.png'
+				}
+			],
+			new IWSize(271,506)
+		),
+		stroke_9:new IWStrokeParts( [ {
+					rect:new IWRect(-2,2,4,251),
+					url:'Guidelines_files/stroke_72.png'
+				},{
+					rect:new IWRect(-2,-2,4,4),
+					url:'Guidelines_files/stroke_73.png'
+				},{
+					rect:new IWRect(2,-2,270,4),
+					url:'Guidelines_files/stroke_74.png'
+				},{
+					rect:new IWRect(272,-2,4,4),
+					url:'Guidelines_files/stroke_75.png'
+				},{
+					rect:new IWRect(272,2,4,251),
+					url:'Guidelines_files/stroke_76.png'
+				},{
+					rect:new IWRect(272,253,4,4),
+					url:'Guidelines_files/stroke_77.png'
+				},{
+					rect:new IWRect(2,253,270,4),
+					url:'Guidelines_files/stroke_78.png'
+				},{
+					rect:new IWRect(-2,253,4,4),
+					url:'Guidelines_files/stroke_79.png'
+				}
+			],
+			new IWSize(274,255)
+		),
+		stroke_7:new IWStrokeParts( [ {
+					rect:new IWRect(-2,2,4,161),
+					url:'Guidelines_files/stroke_56.png'
+				},{
+					rect:new IWRect(-2,-2,4,4),
+					url:'Guidelines_files/stroke_57.png'
+				},{
+					rect:new IWRect(2,-2,273,4),
+					url:'Guidelines_files/stroke_58.png'
+				},{
+					rect:new IWRect(275,-2,4,4),
+					url:'Guidelines_files/stroke_59.png'
+				},{
+					rect:new IWRect(275,2,4,161),
+					url:'Guidelines_files/stroke_60.png'
+				},{
+					rect:new IWRect(275,163,4,4),
+					url:'Guidelines_files/stroke_61.png'
+				},{
+					rect:new IWRect(2,163,273,4),
+					url:'Guidelines_files/stroke_62.png'
+				},{
+					rect:new IWRect(-2,163,4,4),
+					url:'Guidelines_files/stroke_63.png'
+				}
+			],
+			new IWSize(277,165)
+		),
+		stroke_0:new IWStrokeParts( [ {
+			rect:new IWRect(-2,2,4,398),
+					url:'Guidelines_files/stroke.png'
+				},{
+					rect:new IWRect(-2,-2,4,4),
+					url:'Guidelines_files/stroke_1.png'
+				},{
+					rect:new IWRect(2,-2,268,4),
+					url:'Guidelines_files/stroke_2.png'
+				},{
+					rect:new IWRect(270,-2,4,4),
+					url:'Guidelines_files/stroke_3.png'
+				},{
+					rect:new IWRect(270,2,4,398),
+					url:'Guidelines_files/stroke_4.png'
+				},{
+					rect:new IWRect(270,400,4,4),
+					url:'Guidelines_files/stroke_5.png'
+				},{
+					rect:new IWRect(2,400,268,4),
+					url:'Guidelines_files/stroke_6.png'
+				},{
+					rect:new IWRect(-2,400,4,4),
+					url:'Guidelines_files/stroke_7.png'
+				}
+			],
+			new IWSize(272,402)
+		),
+		stroke_1:new IWStrokeParts( [ {
+					rect:new IWRect(-2,2,4,368),
+					url:'Guidelines_files/stroke_8.png'
+				},{
+					rect:new IWRect(-2,-2,4,4),
+					url:'Guidelines_files/stroke_9.png'
+				},{
+					rect:new IWRect(2,-2,268,4),
+					url:'Guidelines_files/stroke_10.png'
+				},{
+					rect:new IWRect(270,-2,4,4),
+					url:'Guidelines_files/stroke_11.png'
+				},{
+					rect:new IWRect(270,2,4,368),
+					url:'Guidelines_files/stroke_12.png'
+				},{
+					rect:new IWRect(270,370,4,4),
+					url:'Guidelines_files/stroke_13.png'
+				},{
+					rect:new IWRect(2,370,268,4),
+					url:'Guidelines_files/stroke_14.png'
+				},{
+					rect:new IWRect(-2,370,4,4),
+					url:'Guidelines_files/stroke_15.png'
+				}
+			],
+			new IWSize(272,372)
+		),
+		stroke_2:new IWStrokeParts( [ {
+					rect:new IWRect(-2,2,4,247),
+					url:'Guidelines_files/stroke_16.png'
+				},{
+					rect:new IWRect(-2,-2,4,4),
+					url:'Guidelines_files/stroke_17.png'
+				},{
+					rect:new IWRect(2,-2,268,4),
+					url:'Guidelines_files/stroke_18.png'
+				},{
+					rect:new IWRect(270,-2,4,4),
+					url:'Guidelines_files/stroke_19.png'
+				},{
+					rect:new IWRect(270,2,4,247),
+					url:'Guidelines_files/stroke_20.png'
+				},{
+					rect:new IWRect(270,249,4,4),
+					url:'Guidelines_files/stroke_21.png'
+				},{
+					rect:new IWRect(2,249,268,4),
+					url:'Guidelines_files/stroke_22.png'
+				},{
+					rect:new IWRect(-2,249,4,4),
+					url:'Guidelines_files/stroke_23.png'
+				}
+			],
+			new IWSize(272,251)
+		)
+	} );
+	registry.applyEffects();
+}
 
 function onPageLoad() {
 	loadMozillaCSS('Guidelines_files/GuidelinesMoz.css')
