23929cbe24c087555b67a01d414bc4ec3855b1e8
Mike Rossetti <vex@trymike.com>
2019-09-09 17:03:48 -0600

    Polishing and add options and contact us.

--- findaword/One_Word_Single_files/One_Word_Single.js
+++ findaword/One_Word_Single_files/One_Word_Single.js
@@ -1,46 +1,5 @@
 // Created by iWeb 3.0.4 local-build-20190822
 
-setTransparentGifURL('Media/transparent.gif');
-
-function applyEffects() {
-	var registry=IWCreateEffectRegistry();
-	registry.registerEffects( { 
-		stroke_0:new IWStrokeParts( [ {
-			rect:new IWRect(-2,2,4,398),
-					url:'Guidelines_files/stroke.png'
-				},{
-					rect:new IWRect(-2,-2,4,4),
-					url:'Guidelines_files/stroke_1.png'
-				},{
-					rect:new IWRect(2,-2,268,4),
-					url:'Guidelines_files/stroke_2.png'
-				},{
-					rect:new IWRect(270,-2,4,4),
-					url:'Guidelines_files/stroke_3.png'
-				},{
-					rect:new IWRect(270,2,4,398),
-					url:'Guidelines_files/stroke_4.png'
-				},{
-					rect:new IWRect(270,400,4,4),
-					url:'Guidelines_files/stroke_5.png'
-				},{
-					rect:new IWRect(2,400,268,4),
-					url:'Guidelines_files/stroke_6.png'
-				},{
-					rect:new IWRect(-2,400,4,4),
-					url:'Guidelines_files/stroke_7.png'
-				}
-			],
-			new IWSize(272,402)
-		),
-	} );
-	registry.applyEffects();
-}
-
-function hostedOnDM() {
-	return false;
-}
-
 function onPageLoad() {
 	loadMozillaCSS('One_Word_Single_files/One_Word_SingleMoz.css')
 	adjustLineHeightIfTooBig('id1');
@@ -57,6 +16,3 @@ function onPageLoad() {
 	performPostEffectsFixups()
 }
 
-function onPageUnload() {
-	Widget.onunload();
-}
