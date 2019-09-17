afbbb64d923135f4e8ce3df4063c2079351244c8
Mike Rossetti <vex@trymike.com>
2019-09-17 17:07:21 -0600

    Populate WordWhisk.

---
+++ wordwhisk/One_Word_Single_files/One_Word_Single.js
@@ -0,0 +1,18 @@
+// Created by iWeb 3.0.4 local-build-20190822
+
+function onPageLoad() {
+	loadMozillaCSS('One_Word_Single_files/One_Word_SingleMoz.css')
+	adjustLineHeightIfTooBig('id1');
+	adjustFontSizeIfTooBig('id1');
+	adjustLineHeightIfTooBig('id2');
+	adjustFontSizeIfTooBig('id2');
+	adjustLineHeightIfTooBig('id3');
+	adjustFontSizeIfTooBig('id3');
+	adjustLineHeightIfTooBig('id4');
+	adjustFontSizeIfTooBig('id4');
+	Widget.onload();
+	fixAllIEPNGs('Media/transparent.gif');
+	applyEffects()
+	performPostEffectsFixups()
+}
+
